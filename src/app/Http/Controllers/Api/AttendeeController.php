f6466ebbb5b9c6c29d60a20d4f30262373064bc0
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-03-12 20:49:19 -0300

    Adicionando relações de user com attendee

--- src/app/Http/Controllers/Api/AttendeeController.php
+++ src/app/Http/Controllers/Api/AttendeeController.php
@@ -4,18 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
+    use CanLoadRelationships;
+    private array $relations = ['user'];
+
     /**
      * Display a listing of the resource.
      */
     public function index(Event $event)
     {
-        $attendees = $event->attendees()->latest();
+        $attendees = $this->loadRelationships($event->attendees()->latest());
 
         return AttendeeResource::collection(
             $attendees->paginate()
@@ -39,7 +43,9 @@ class AttendeeController extends Controller
      */
     public function show(Event $event, Attendee $attendee)
     {
-        return new AttendeeResource($attendee);
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
     }
 
     /**
